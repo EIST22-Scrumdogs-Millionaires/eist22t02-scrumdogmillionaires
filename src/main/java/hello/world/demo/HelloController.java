531d7a66f4b70b11b476de5b86dada89b70fc9ff
Stefan Eibl <ge97dek@mytum.de>
2022-04-22 15:21:42 +0200

    Exercise-Template pushed by Artemis

---
+++ src/main/java/hello/world/demo/HelloController.java
@@ -0,0 +1,14 @@
+package hello.world.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("sayHello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
