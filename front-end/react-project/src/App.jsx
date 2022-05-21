9f35e70ef999833b97e21417a7c5b2315589bbfc
16domsim <55878762+16domsim@users.noreply.github.com>
2022-05-21 17:27:34 +0200

    Front- Backend connection

--- front-end/react-project/src/App.jsx
+++ front-end/react-project/src/App.jsx
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from 'react-hook-use-state';
+
+
 
 function App() {
+
+  const [json, setJSON] = useState(null);
+
+
+        fetch('http://localhost:8080/restaurants')
+        .then(response => response.json())
+        .then(data => setJSON(JSON.stringify(data)));
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Get JSON from server: {json}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+
+
 
 export default App;
