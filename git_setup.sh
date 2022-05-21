8b9edea2b8b6009d75027bb47015f6f09b92d82d
Aamin Gem <gem.aamin@gmail.com>
2022-05-21 23:30:28 +0200

    Fixed git_setup

--- git_setup.sh
+++ git_setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git remote add bitbucket ssh://git@bitbucket.ase.in.tum.de:7999/eist22t02/eist22t02-scrumdogmillionaires.git && git config --global alias.pushall '!git remote | xargs -L1 git push --all'
+sudo git config --system receive.denyNonFastForwards true && git remote add bitbucket ssh://git@bitbucket.ase.in.tum.de:7999/eist22t02/eist22t02-scrumdogmillionaires.git && git config --global alias.pushall '!git remote | xargs -L1 git push --all'
