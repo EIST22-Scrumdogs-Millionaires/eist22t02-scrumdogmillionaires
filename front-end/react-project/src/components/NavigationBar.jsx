6f4d8c7f48578c86c50caf4783a5f40c3f681ead
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-05-22 00:03:47 +0200

    update navigation bar

--- front-end/react-project/src/components/NavigationBar.jsx
+++ front-end/react-project/src/components/NavigationBar.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import SearchBox from "./SearchBox";
-export default function NavigationBar(){
-    return <AppBar position="static" color="primary">
-    <Toolbar>
-<Typography variant="h6" component="div">Restaurant Reservation App</Typography>
-
-    </Toolbar>
-  </AppBar>
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Stack,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+export default function NavigationBar() {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+         {
+             //TODO add app name?
+         }
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <IconButton size="large" color="inherit">
+            <DarkModeIcon />
+          </IconButton>
+          <Button color="inherit">Login</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 }
