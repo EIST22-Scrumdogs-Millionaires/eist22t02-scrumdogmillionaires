05785d1313a7b3b935eae03bbe5cc3c59cee2079
africaro <93818508+africaro@users.noreply.github.com>
2022-06-06 21:15:23 +0200

    updated restaurant overview

--- src/main/java/hello/world/demo/restaurant/RestaurantOverview.java
+++ src/main/java/hello/world/demo/restaurant/RestaurantOverview.java
@@ -1,4 +1,16 @@
 package hello.world.demo.restaurant;
 
+import java.util.List;
+
 public class RestaurantOverview {
+    private static String filterType;
+    private static List<Restaurant> restaurants;
+
+    public static void selectRestaurant(Restaurant r) {
+
+    }
+
+    public static String getFilterType() {
+        return filterType;
+    }
 }
