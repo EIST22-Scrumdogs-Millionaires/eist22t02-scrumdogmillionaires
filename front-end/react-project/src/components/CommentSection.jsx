a76717343a7fe1f661d4adb9dc961b05c5ff7b78
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-06-14 19:18:19 +0200

    update commentSection to make it responsible

--- front-end/react-project/src/components/CommentSection.jsx
+++ front-end/react-project/src/components/CommentSection.jsx
@@ -8,63 +8,75 @@ export default function CommentSection(props) {
   const [value, setValue] = React.useState(0);
   return (
     <div className="comment-section">
-      <Stack direction="row" spacing={3} style={{ margin: "15px 0px" }}>
-        <h3 style={{ margin: "auto" }}>Comments:</h3>
-        <div style={{ flex: 1 }} />
-        <h3>Total:</h3>
-        <div style={{ margin: "auto 15px" }}>
-          <Rating name="read-only" value={3} readOnly />
-        </div>
-      </Stack>
+      <div style={{ margin: "15px 0px" }}>
+        <Stack direction="row" spacing={3}>
+          <div style={{ flex: 1 }} />
+          <h3>Total:</h3>
+          <div style={{ margin: "auto 15px" }}>
+            <Rating name="read-only" value={3} readOnly />
+          </div>
+        </Stack>
+      </div>
+
       <Stack
         spacing={2}
         divider={<Divider orientation="horizontal" flexItem />}
       >
         <div className="comment">
-          <Stack direction="row" spacing={3}>
-            <ProfilPictureComments name="Max Mustermann" />
-
-            <div style={{ textAlign: "left", margin: "auto 0px auto 15px" }}>
+          <Stack spacing={1}>
+            <Stack direction="row" spacing={1}>
+              <ProfilPictureComments name="Max Mustermann" />
+              <strong style={{ margin: "auto 0 auto 15px" }}>
+                Max Mustermann:
+              </strong>
+              <div style={{ flex: 1 }} />
+              <div style={{ margin: "auto 5px auto 0" }}>
+                <Rating name="read-only" value={3} readOnly />
+              </div>
+            </Stack>
+            <div style={{ textAlign: "left", margin: "15px 0 0 0" }}>
               <Typography>
-                <strong>Max Mustermann:</strong><br/>It was good It was good It was good It was good
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
               </Typography>
             </div>
-            <div style={{ flex: 1 }} />
-            <div style={{ margin: "auto 5px" }}>
-              <Rating name="read-only" value={3} readOnly />
-            </div>
           </Stack>
         </div>
 
         <div className="comment">
-          <Stack direction="row" spacing={3}>
-            <ProfilPictureComments name="Test lol" />
-
-            <div
-              className="test"
-              style={{ textAlign: "left", margin: "auto 10px" }}
-            >
-              <Typography>It was bad </Typography>
-            </div>
-            <div style={{ flex: 1 }} />
-            <div style={{ margin: "auto" }}>
-              <Rating name="read-only" value={1} readOnly />
+          <Stack spacing={1}>
+            <Stack direction="row" spacing={1}>
+              <ProfilPictureComments name="Erika Mustermann:" />
+              <strong style={{ margin: "auto 0 auto 15px" }}>
+                Erika Mustermann:
+              </strong>
+              <div style={{ flex: 1 }} />
+              <div style={{ margin: "auto 5px auto 0" }}>
+                <Rating name="read-only" value={3} readOnly />
+              </div>
+            </Stack>
+            <div style={{ textAlign: "left", margin: "15px 0 0 0" }}>
+              <Typography>
+              Lorem ipsum dolor sit amet.
+              </Typography>
             </div>
           </Stack>
         </div>
         <div className="comment">
-          <Stack direction="row" spacing={3}>
-            <ProfilPictureComments name="Cremers" />
-
-            <div
-              className="test"
-              style={{ textAlign: "left", margin: "auto 10px" }}
-            >
-              <Typography>It was good</Typography>
-            </div>
-            <div style={{ flex: 1 }} />
-            <div style={{ margin: "auto" }}>
-              <Rating name="read-only" value={5} readOnly />
+          <Stack spacing={1}>
+            <Stack direction="row" spacing={1}>
+              <ProfilPictureComments name="Anonymous" />
+              <strong style={{ margin: "auto 0 auto 15px" }}>
+              Anonymous User:
+              </strong>
+              <div style={{ flex: 1 }} />
+              <div style={{ margin: "auto 5px auto 0" }}>
+                <Rating name="read-only" value={3} readOnly />
+              </div>
+            </Stack>
+            <div style={{ textAlign: "left", margin: "15px 0 0 0" }}>
+              <Typography>
+              Lorem ipsum dolor sit amet.
+              </Typography>
             </div>
           </Stack>
         </div>
@@ -76,52 +88,56 @@ export default function CommentSection(props) {
       >
         <h3 style={{ margin: "auto" }}>Your comment:</h3>
         <div style={{ flex: 1 }} />
-       
       </Stack>
 
       <div className="comment">
         <Stack direction="row" spacing={3}>
-          <div style={{ textAlign: "left", margin: "auto 10px", flex:1 }}>
+          <div style={{ textAlign: "left", margin: "auto 10px", flex: 1 }}>
             <Box
               sx={{
                 width: "100%",
               }}
             >
               <TextField
+                color="secondary"
+                required
+                id="standard-basic"
+                label="Name"
+                variant="standard"
+              />
+
+              <p />
+              <TextField
                 fullWidth
-                label="Comment..."
+                required
+                label="Comment"
                 variant="standard"
                 color="secondary"
               />
             </Box>
           </div>
-          <div style={{ margin: "auto" }}>
-            <Rating
-              name="simple-controlled"
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }}
-            />
-          </div>
-          <div style={{ margin: "auto 0 auto 15px" }}>
-            
-          </div>
         </Stack>
-        
+        <div style={{ textAlign: "left", margin: "30px 0 5px 10px" }}>
+          <Rating
+            name="simple-controlled"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          />
+        </div>
       </div>
       <Stack
         direction="row"
         spacing={3}
         style={{ margin: "10px 0px 10px 0px" }}
       >
-      <div style={{ flex: 1 }} />
-      <Button color="secondary" variant="contained" component={Link}>
-              Post
-            </Button>
+        <div style={{ flex: 1 }} />
+
+        <Button color="secondary" variant="contained" component={Link}>
+          Post
+        </Button>
       </Stack>
-      
-      </div>
-   
+    </div>
   );
 }
