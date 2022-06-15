e7c65c278ececd247b9c2a2059ba6fb8650945d1
africaro <93818508+africaro@users.noreply.github.com>
2022-06-15 21:49:28 +0200

    xx

--- src/main/java/hello/world/demo/EmailService.java
+++ src/main/java/hello/world/demo/EmailService.java
@@ -3,10 +3,14 @@ package hello.world.demo;
 import hello.world.demo.restaurant.Reservation;
 import hello.world.demo.restaurant.Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-
+import java.util.Properties;
 
 
 @Component
@@ -14,9 +18,34 @@ public class EmailService {
 
 
     @Autowired
-    //private JavaMailSender mailsender;
+    private JavaMailSender mailSender;
 
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@scrumdogmillionaires.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
 
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 
 
     public static void confirmReservation(Reservation reservation) {
