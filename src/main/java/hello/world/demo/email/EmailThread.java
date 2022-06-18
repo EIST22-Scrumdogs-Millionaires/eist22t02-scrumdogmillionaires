2d3589ece60afbca61f7d5dd17d5ddc0e1d97ed9
16domsim <55878762+16domsim@users.noreply.github.com>
2022-06-18 12:52:55 +0200

    Email Class

---
+++ src/main/java/hello/world/demo/email/EmailThread.java
@@ -0,0 +1,18 @@
+package hello.world.demo.email;
+
+
+
+
+public class EmailThread  extends Thread{
+
+    
+
+    @Override 
+    public void run(){
+
+
+
+    }
+
+  
+}
---
+++ src/main/java/hello/world/demo/restaurant/Email.java
@@ -0,0 +1,20 @@
+package hello.world.demo.restaurant;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Email {
+
+    private String to;
+
+    private String subject;
+
+    private String text;
+
+    private LocalDate sendDate;
+
+    private LocalTime sendTime;
+
+
+}
