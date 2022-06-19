a28cdbfa7f369fe79d8fe1674499e76d06acc873
16domsim <55878762+16domsim@users.noreply.github.com>
2022-06-19 11:55:06 +0200

    Update DemoApplication.java

--- src/main/java/hello/world/demo/DemoApplication.java
+++ src/main/java/hello/world/demo/DemoApplication.java
@@ -11,11 +11,3 @@ public class DemoApplication {
 	}
 
 }
-
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-@Override
-protected void configure(HttpSecurity http) throws Exception {
-    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-}
