5ca50c8a109c7895541651c2a5f89ffc9c840af2
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-06-24 13:06:30 +0200

    change image layout

--- front-end/react-project/src/pages/DetailSearchComponent.jsx
+++ front-end/react-project/src/pages/DetailSearchComponent.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import NavigationBar from "../components/NavigationBar";
-import { Container } from "@mui/material";
+import { Container, Grid, ImageList, ImageListItem } from "@mui/material";
 import CommentSection from "../components/CommentSection";
 import ReservateComponent from "../components/ReservateComponent";
 import IconButton from "@mui/material/IconButton";
@@ -25,8 +25,6 @@ export default function DetailSearchComponent() {
   if (data == null || data == undefined) {
     return <div>Loading...</div>;
   } else {
-    
-
     for (let [key, value] of Object.entries(data)) {
       console.log(`${key}: ${value}`);
     }
@@ -45,11 +43,22 @@ export default function DetailSearchComponent() {
                 <h1>{data.name}</h1>
                 {data.description}
                 <p>
-                {data.priceCategory === undefined ? null :  (<span><em>Price Category: </em>{data.priceCategory}</span>)
-                  }
-                  {data.priceCategory === undefined || data.restaurantType === undefined ? null : <span>,</span>}
-                 {data.restaurantType === undefined ? null : (<span><em>Type: </em>
-                  {data.restaurantType}</span>)}
+                  {data.priceCategory === undefined ? null : (
+                    <span>
+                      <em>Price Category: </em>
+                      {data.priceCategory}
+                    </span>
+                  )}
+                  {data.priceCategory === undefined ||
+                  data.restaurantType === undefined ? null : (
+                    <span>,</span>
+                  )}
+                  {data.restaurantType === undefined ? null : (
+                    <span>
+                      <em>Type: </em>
+                      {data.restaurantType}
+                    </span>
+                  )}
                 </p>
               </div>
             </div>
@@ -79,17 +88,37 @@ export default function DetailSearchComponent() {
               )}
             </div>
             <div className="picture-wrapper">
+            
+              <ImageList
+                sx={{
+                  width: {
+                    xs: 300,
+                    sm: 500,
+                    md: 800,     
+                  },
+                  height: 450,
+                }}
+                variant="woven"
+                cols={3}
+                gap={8}
+                style={{margin: "auto"}}
+              >
                 {data.pictures.map((picture) => (
+                  <ImageListItem>
                     <img
-                        src={`${picture+apikey}`}
-                        alt="restaurant picture"
-                        className="picture"
+                      src={`${picture + apikey}`}
+                      alt="restaurant picture"
+                      className="picture"
+                      loading="lazy"
                     />
+                  </ImageListItem>
                 ))}
+              </ImageList>
             </div>
             <div>
-            {data.reviews ? <ReservateComponent restaurant={data}></ReservateComponent> : null}
-             
+              {data.reviews ? (
+                <ReservateComponent restaurant={data}></ReservateComponent>
+              ) : null}
             </div>
             <CommentSection restaurant={data} />
           </Container>
